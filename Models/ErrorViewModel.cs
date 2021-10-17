6457041e20073eb29e3d2a617f6d1566dc862f66
Marcin Pająk <mpajak@wsei.edu.pl>
2021-10-17 09:52:29 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibApp_Gr2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
